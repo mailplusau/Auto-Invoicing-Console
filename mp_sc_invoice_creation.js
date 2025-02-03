6ea942e1ea7f0bd3a47f226fc41773fedff76b46
ankith88 <ankith88@gmail.com>
2025-02-03 11:33:31 +1100

    CU-86cxgp10j

--- mp_sc_invoice_creation.js
+++ mp_sc_invoice_creation.js
@@ -350,15 +350,15 @@ function invoiceCreation() {
 								nlapiLoadRecord("partner", franchisee).getFieldValue("location")
 							);
 							// recInvoice.setFieldValue('trandate', invoice_date());
-							recInvoice.setFieldValue("trandate", "31/12/2024");
+							recInvoice.setFieldValue("trandate", "31/01/2025");
 							recInvoice.setFieldValue("custbody_dont_update_trandate", "T");
 							recInvoice.setFieldValue(
 								"custbody_inv_date_range_from",
-								"1/12/2024"
+								"1/1/2025"
 							);
 							recInvoice.setFieldValue(
 								"custbody_inv_date_range_to",
-								"31/12/2024"
+								"31/1/2025"
 							);
 
 							recInvoice.setFieldValue("partner", franchisee);
